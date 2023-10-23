17df8d41304e1e4935fe368e5953bbee20bdd887
vunt378 <55792941+tuanvuPresent@users.noreply.github.com>
2023-10-23 22:31:48 +0700

    Update views.py

--- apps/message/views.py
+++ apps/message/views.py
@@ -19,22 +19,8 @@ class MessageModelViewSet(BaseModelViewSet):
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-
         message = serializer.validated_data.get('message')
         room = serializer.validated_data.get('room')
-        self._send_message_ws(message=message, channel=room)
-
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-        
-        
-    def _send_message_ws(self, message, channel):
-        channel_layer = get_channel_layer()
-        async_to_sync(channel_layer.group_send)(
-            channel,
-            {
-                'type': 'chat_message',
-                'message': message
-            }
-        )
